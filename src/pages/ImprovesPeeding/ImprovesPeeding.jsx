c01d9d952ab951ab3c6dae100cea5275d1e935e6
gustaVASSIO <guguzim.anacleto@gmail.com>
2023-07-06 10:04:16 -0300

    Conectando com api pagina de improves peending

--- src/pages/ImprovesPeeding/ImprovesPeeding.jsx
+++ src/pages/ImprovesPeeding/ImprovesPeeding.jsx
@@ -1,56 +1,54 @@
-import  './ImprovesPeending.css'
+import './ImprovesPeending.css'
 import Title from '../../components/Title';
+import { toast } from "react-toastify";
+import api from '../../services/api'
+import { useEffect, useState } from 'react';
 
-export default () =>{
-    return(
-        <>
-            <Title name="Improvements"></Title>
+export default () => {
+    const [improvesPeending, setImprovesPeending] = useState([])
 
-            <div className="peeding-main">
-            <div className="improve-container">
-                   <div className='container-1'>
-                        <h1 className='name-requester'>Name Requester / Department</h1>
-                        <h3 className='summary'>Summary improve here</h3>
-                        <p className=''>01/01/0001</p>
-                        <div className="buttons">
-                            <button className='btn btn-edit'>See and Edit</button>
-                            <button className='btn btn-approve'>Approve</button>
-                        </div>
-                   </div>
-                   <div className='container-2'>
-                        <div className="peeding-status">Peeding</div>
-                   </div>
-            </div>
-            <div className="improve-container">
-                   <div className='container-1'>
-                        <h1 className='name-requester'>Name Requester / Department</h1>
-                        <h3 className='summary'>Summary improve here</h3>
-                        <p className=''>01/01/0001</p>
-                        <div className="buttons">
-                            <button className='btn btn-edit'>See and Edit</button>
-                            <button className='btn btn-approve'>Approve</button>
-                        </div>
-                   </div>
-                   <div className='container-2'>
-                        <div className="peeding-status">Peeding</div>
-                   </div>
-            </div>
-            <div className="improve-container">
-                   <div className='container-1'>
-                        <h1 className='name-requester'>Name Requester / Department</h1>
-                        <h3 className='summary'>Summary improve here</h3>
-                        <p className=''>01/01/0001</p>
-                        <div className="buttons">
-                            <button className='btn btn-edit'>See and Edit</button>
-                            <button className='btn btn-approve'>Approve</button>
-                        </div>
-                   </div>
-                   <div className='container-2'>
-                        <div className="peeding-status">Peeding</div>
-                   </div>
-            </div>
-        </div>
+    useEffect(() => {
+        loadImprovementsPeending()
+    }, [])
+
+    function loadImprovementsPeending(){
+        api.get('/improvement/peending')
+        .then((response) => {
+            setImprovesPeending(response.data)
+            console.log(response.data)
+        }).catch(e => {
+            toast.error('Não foi possível se conectar')
+        })
+    }
+
+    function handleApprove(id){
         
+    }
+    return (
+        <>
+            <Title name="Improvements Peendings"></Title>
+            {
+                improvesPeending.map((improve, i) => {
+                    return <div key={i} className="peeding-main">
+                        <div className="improve-container">
+                            <div className='container-1'>
+                                <h1 className='name-requester'>{improve.signature} / {improve.department.name}</h1>
+                                <h3 className='summary'>{improve.summary}</h3>
+                                <p className=''>{improve.dueDate}</p>
+                                <div className="buttons">
+                                    <button className='btn btn-edit'>See and Edit</button>
+                                    <button className='btn btn-approve' onClick="" >Approve</button>
+                                </div>
+                            </div>
+                            <div className='container-2'>
+                                <div className="peeding-status">Peeding</div>
+                            </div>
+                        </div>
+                    </div>
+                })
+            }
+
+
         </>
     )
 }
